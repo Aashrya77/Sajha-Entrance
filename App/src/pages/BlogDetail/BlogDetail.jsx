b168fb8295f1ba960cfcc21429de0e7a70cc977d
Mukesh Bhat <mukeshbhat977@gmail.com>
2026-03-03 12:29:58 +0545

    blog updated

--- App/src/pages/BlogDetail/BlogDetail.jsx
+++ App/src/pages/BlogDetail/BlogDetail.jsx
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { blogAPI } from '../../api/services';
 import Loader from '../../components/Loader/Loader';
+import '../../../public/css/blog.css';
 
 const BlogDetail = () => {
   const { id } = useParams();
   const [blogData, setBlogData] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [shareModal, setShareModal] = useState({ isOpen: false, blogTitle: '', blogUrl: '' });
 
   useEffect(() => {
     fetchBlogDetail();
@@ -25,6 +27,54 @@ const BlogDetail = () => {
     }
   };
 
+  const openShareModal = () => {
+    const blogUrl = window.location.href;
+    const blogData_ = blogData.blogData;
+    setShareModal({
+      isOpen: true,
+      blogTitle: blogData_.blogTitle,
+      blogUrl: blogUrl
+    });
+  };
+
+  const closeShareModal = () => {
+    setShareModal({ isOpen: false, blogTitle: '', blogUrl: '' });
+  };
+
+  const handleShare = (platform) => {
+    const { blogTitle, blogUrl } = shareModal;
+    const text = `Check out this blog: ${blogTitle}`;
+    let shareUrl = '';
+
+    switch(platform) {
+      case 'facebook':
+        shareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(blogUrl)}`;
+        break;
+      case 'twitter':
+        shareUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(text)}&url=${encodeURIComponent(blogUrl)}`;
+        break;
+      case 'linkedin':
+        shareUrl = `https://www.linkedin.com/sharing/share-offsite/?url=${encodeURIComponent(blogUrl)}`;
+        break;
+      case 'whatsapp':
+        shareUrl = `https://wa.me/?text=${encodeURIComponent(text + ' ' + blogUrl)}`;
+        break;
+      case 'telegram':
+        shareUrl = `https://t.me/share/url?url=${encodeURIComponent(blogUrl)}&text=${encodeURIComponent(text)}`;
+        break;
+      case 'pinterest':
+        shareUrl = `https://pinterest.com/pin/create/button/?url=${encodeURIComponent(blogUrl)}`;
+        break;
+      default:
+        return;
+    }
+
+    if (shareUrl) {
+      window.open(shareUrl, '_blank', 'width=600,height=400');
+      closeShareModal();
+    }
+  };
+
   if (loading) {
     return <div className="container mt-5 pt-5 d-flex justify-content-center"><Loader /></div>;
   }
@@ -37,7 +87,7 @@ const BlogDetail = () => {
 
   return (
     <div className="blog-detail mt-5 pt-5">
-      <div className="container-fluid">
+      <div className="container">
         <div className="row">
           <div className="col-lg-8">
             <article className="blog-article">
@@ -45,15 +95,23 @@ const BlogDetail = () => {
               
               <div className="blog-meta mb-4">
                 <span className="blog-date">
-                  <i className="fa-solid fa-calendar me-2"></i>
+                  <i className="fa-solid fa-calendar"></i>
                   {new Date(blog.createdAt).toLocaleDateString()}
                 </span>
                 {blog.author && (
-                  <span className="blog-author ms-3">
-                    <i className="fa-solid fa-user me-2"></i>
+                  <span className="blog-author">
+                    <i className="fa-solid fa-user"></i>
                     {blog.author}
                   </span>
                 )}
+                <button 
+                  className="article-share-btn"
+                  onClick={openShareModal}
+                  title="Share this blog"
+                >
+                  <i className="fa-solid fa-share-nodes"></i>
+                  Share
+                </button>
               </div>
 
               {blog.blogImage && (
@@ -92,6 +150,70 @@ const BlogDetail = () => {
           </div>
         </div>
       </div>
+
+      {/* Share Modal */}
+      {shareModal.isOpen && (
+        <div className="share-modal" onClick={closeShareModal}>
+          <div className="share-modal-content" onClick={(e) => e.stopPropagation()}>
+            <div className="share-modal-header">
+              <h4>Share this Blog</h4>
+              <button className="share-modal-close" onClick={closeShareModal}>
+                ×
+              </button>
+            </div>
+            <div className="share-options">
+              <button 
+                className="share-option share-facebook"
+                onClick={() => handleShare('facebook')}
+                title="Share on Facebook"
+              >
+                <i className="fa-brands fa-facebook-f"></i>
+                <span>Facebook</span>
+              </button>
+              <button 
+                className="share-option share-twitter"
+                onClick={() => handleShare('twitter')}
+                title="Share on Twitter"
+              >
+                <i className="fa-brands fa-twitter"></i>
+                <span>Twitter</span>
+              </button>
+              <button 
+                className="share-option share-linkedin"
+                onClick={() => handleShare('linkedin')}
+                title="Share on LinkedIn"
+              >
+                <i className="fa-brands fa-linkedin-in"></i>
+                <span>LinkedIn</span>
+              </button>
+              <button 
+                className="share-option share-whatsapp"
+                onClick={() => handleShare('whatsapp')}
+                title="Share on WhatsApp"
+              >
+                <i className="fa-brands fa-whatsapp"></i>
+                <span>WhatsApp</span>
+              </button>
+              <button 
+                className="share-option share-telegram"
+                onClick={() => handleShare('telegram')}
+                title="Share on Telegram"
+              >
+                <i className="fa-brands fa-telegram"></i>
+                <span>Telegram</span>
+              </button>
+              <button 
+                className="share-option share-pinterest"
+                onClick={() => handleShare('pinterest')}
+                title="Share on Pinterest"
+              >
+                <i className="fa-brands fa-pinterest-p"></i>
+                <span>Pinterest</span>
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- App/src/pages/Blogs/Blogs.jsx
+++ App/src/pages/Blogs/Blogs.jsx
@@ -11,10 +11,11 @@ const Blogs = () => {
   const [previousPage, setPreviousPage] = useState(0);
   const [nextPage, setNextPage] = useState(0);
   const [searchTerm, setSearchTerm] = useState('');
+  const [shareModal, setShareModal] = useState({ isOpen: false, blogId: null, blogTitle: '', blogUrl: '' });
 
   useEffect(() => {
     fetchBlogs();
-  }, [currentPage]); // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentPage]);
 
   const fetchBlogs = async () => {
     try {
@@ -37,55 +38,114 @@ const Blogs = () => {
     fetchBlogs();
   };
 
+  const openShareModal = (e, blog) => {
+    e.preventDefault();
+    e.stopPropagation(); // Prevents the Link click
+    const blogUrl = `${window.location.origin}/blog/${blog._id}`;
+    setShareModal({
+      isOpen: true,
+      blogId: blog._id,
+      blogTitle: blog.blogTitle,
+      blogUrl: blogUrl
+    });
+  };
+
+  const closeShareModal = () => {
+    setShareModal({ isOpen: false, blogId: null, blogTitle: '', blogUrl: '' });
+  };
+
+  const handleShare = (platform) => {
+    const { blogTitle, blogUrl } = shareModal;
+    const text = `Check out this blog: ${blogTitle}`;
+    let shareUrl = '';
+
+    switch(platform) {
+      case 'facebook': shareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(blogUrl)}`; break;
+      case 'twitter': shareUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(text)}&url=${encodeURIComponent(blogUrl)}`; break;
+      case 'linkedin': shareUrl = `https://www.linkedin.com/sharing/share-offsite/?url=${encodeURIComponent(blogUrl)}`; break;
+      case 'whatsapp': shareUrl = `https://wa.me/?text=${encodeURIComponent(text + ' ' + blogUrl)}`; break;
+      case 'telegram': shareUrl = `https://t.me/share/url?url=${encodeURIComponent(blogUrl)}&text=${encodeURIComponent(text)}`; break;
+      case 'pinterest': shareUrl = `https://pinterest.com/pin/create/button/?url=${encodeURIComponent(blogUrl)}`; break;
+      default: return;
+    }
+
+    if (shareUrl) {
+      window.open(shareUrl, '_blank', 'width=600,height=400');
+      closeShareModal();
+    }
+  };
+
   if (loading) {
     return <div className="container mt-5 pt-5 d-flex justify-content-center"><Loader /></div>;
   }
 
   return (
-    <div className="blogs mt-5 pt-5" >
-      <div className="container-fluid">
-        <h1 className="text-uppercase mb-4 text-center" style={{fontWeight: 900, color: 'var(--primary-orange)'}}>
-          OUR <span style={{color: 'var(--primary-black)'}}>BLOGS</span>
+    <div className="blogs mt-5 pt-5">
+      <div className="container-fluid px-4">
+        <h1 className="text-uppercase mb-4 text-center" style={{ fontWeight: 900, color: 'var(--primary-orange)' }}>
+          OUR <span style={{ color: 'var(--primary-black)' }}>BLOGS</span>
         </h1>
 
         {/* Search Bar */}
-        <div className="row justify-content-center mb-4">
-          <div className="col-md-6">
-            <form onSubmit={handleSearch} className="d-flex">
+        <div className="row justify-content-center mb-5">
+          <div className="col-md-5">
+            <form onSubmit={handleSearch} className="d-flex shadow-sm rounded">
               <input
                 type="text"
-                className="form-control me-2"
-                placeholder="Search blogs..."
+                className="form-control me-0 border-end-0"
+                style={{ borderRadius: '8px 0 0 8px' }}
+                placeholder="Search articles..."
                 value={searchTerm}
                 onChange={(e) => setSearchTerm(e.target.value)}
               />
-              <button type="submit" className="btn btn-primary">
+              <button type="submit" className="btn btn-primary" style={{ borderRadius: '0 8px 8px 0' }}>
                 <i className="fa-solid fa-search"></i>
               </button>
             </form>
           </div>
         </div>
 
-        <div className="row g-4 mt-4">
+        {/* 4-Column Grid: Updated bootstrap classes */}
+        <div className="row g-4 mt-2">
           {blogs.map((blog) => (
-            <div key={blog._id} className="col-12 col-md-6 col-lg-4">
+            <div key={blog._id} className="col-12 col-sm-6 col-lg-4 col-xl-3">
               <Link to={`/blog/${blog._id}`} className="blog-card-link">
                 <div className="blog-card">
-                  {blog.blogImage && (
-                    <div className="blog-card-image">
-                      <img src={blog.blogImage} alt={blog.blogTitle} />
-                    </div>
-                  )}
+                  <div className="blog-card-image">
+                    <img 
+                      src={blog.blogImage || '/default-blog.jpg'} 
+                      alt={blog.blogTitle} 
+                      onError={(e) => { e.target.src = 'https://placehold.co/400x240?text=No+Image'; }}
+                    />
+                  </div>
+                  
                   <div className="blog-card-content">
                     <h5 className="blog-title">{blog.blogTitle}</h5>
                     <p className="blog-excerpt">
-                      {blog.blogContent ? blog.blogContent.substring(0, 150) + '...' : ''}
+                      {blog.blogContent ? blog.blogContent.replace(/<[^>]*>?/gm, '').substring(0, 90) + '...' : ''}
                     </p>
+                    
+                    {/* Meta Row: Date Left, Share Right */}
                     <div className="blog-meta">
-                      <span className="blog-date">
-                        <i className="fa-solid fa-calendar me-2"></i>
-                        {new Date(blog.createdAt).toLocaleDateString()}
-                      </span>
+                      <div className="blog-meta-left">
+                        <span className="blog-date">
+                          <i className="fa-solid fa-calendar-days"></i>
+                          {new Date(blog.createdAt).toLocaleDateString()}
+                        </span>
+                      </div>
+                      <button 
+                        className="btn-share"
+                        onClick={(e) => openShareModal(e, blog)}
+                      >
+                        <i className="fa-solid fa-share-nodes"></i>Share
+                      </button>
+                    </div>
+
+                    {/* Read More: Aligned Right */}
+                    <div className="text-end mt-2">
+                       <span className="btn-read-more">
+                         Read More <i className="fa-solid fa-arrow-right"></i>
+                       </span>
                     </div>
                   </div>
                 </div>
@@ -96,26 +156,46 @@ const Blogs = () => {
 
         {/* Pagination */}
         <div className="pagination-wrapper mt-5">
-          <nav aria-label="Page navigation">
-            <ul className="pagination justify-content-center">
-              {previousPage > 0 && (
-                <li className="page-item">
-                  <button className="page-link" onClick={() => setCurrentPage(previousPage)}>
-                    Previous
-                  </button>
-                </li>
-              )}
-              {nextPage > 0 && (
-                <li className="page-item">
-                  <button className="page-link" onClick={() => setCurrentPage(nextPage)}>
-                    Next
-                  </button>
-                </li>
-              )}
-            </ul>
-          </nav>
+          <ul className="pagination">
+            {previousPage > 0 && (
+              <li className="page-item">
+                <button className="page-link" onClick={() => setCurrentPage(previousPage)}>Previous</button>
+              </li>
+            )}
+            {nextPage > 0 && (
+              <li className="page-item">
+                <button className="page-link" onClick={() => setCurrentPage(nextPage)}>Next</button>
+              </li>
+            )}
+          </ul>
         </div>
       </div>
+
+      {/* Share Modal remains same logic, just ensure CSS matches */}
+      {shareModal.isOpen && (
+        <div className="share-modal" onClick={closeShareModal}>
+          <div className="share-modal-content" onClick={(e) => e.stopPropagation()}>
+            <div className="share-modal-header">
+              <h4>Share this Blog</h4>
+              <button className="share-modal-close" onClick={closeShareModal}>&times;</button>
+            </div>
+            <div className="share-options">
+              <button className="share-option" onClick={() => handleShare('facebook')}>
+                <i className="fa-brands fa-facebook" style={{color: '#1877f2'}}></i>
+                <span>Facebook</span>
+              </button>
+              <button className="share-option" onClick={() => handleShare('whatsapp')}>
+                <i className="fa-brands fa-whatsapp" style={{color: '#25d366'}}></i>
+                <span>WhatsApp</span>
+              </button>
+              <button className="share-option" onClick={() => handleShare('twitter')}>
+                <i className="fa-brands fa-twitter" style={{color: '#1da1f2'}}></i>
+                <span>Twitter</span>
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
