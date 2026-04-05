ec7b701b3e0c40b5f31e930b407cdb477821b41f
Mukesh Bhat <mukeshbhat977@gmail.com>
2026-04-05 23:48:37 +0545

    server file

--- Server/server.js
+++ Server/server.js
@@ -70,7 +70,7 @@ const imageDirectoryMappings = [
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "http://localhost:5000",],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
