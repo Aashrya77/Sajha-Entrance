6563f47e4c532c43cdf25116320277a1b96b77c0
Aashrya77 <aashryasigdel0@gmail.com>
2026-04-06 11:22:42 +0545

    uploads

--- Server/server.js
+++ Server/server.js
@@ -35,6 +35,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const publicDirectory = path.join(__dirname, "public");
 
+app.use("/uploads", express.static(path.join(publicDirectory, "uploads")));
+
 
 // ================= IMAGE MAPPINGS =================
 const imageDirectoryMappings = [
