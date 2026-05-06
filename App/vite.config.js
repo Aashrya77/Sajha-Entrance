ee317b535689b50c83f91163171ce536ff95dbc3
Aashrya77 <aashryasigdel0@gmail.com>
2026-05-06 21:50:09 +0545

    fix

--- App/vite.config.js
+++ App/vite.config.js
@@ -61,8 +61,7 @@ export default defineConfig(({ mode }) => {
       }
     },
     build: {
-      outDir: '../Server/public',
-      emptyOutDir: false,
+      outDir: 'dist',
       sourcemap: false
     }
   };
